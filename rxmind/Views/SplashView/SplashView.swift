e751dba1ca53e86953612021c17e01ed4a677233
Shanre Scheepers <21100387@virtualwindow.co.za>
2023-09-03 17:59:26 +0200

    // app flow done - initialising ios app

--- rxmind/Views/SplashView/SplashView.swift
+++ rxmind/Views/SplashView/SplashView.swift
@@ -20,34 +20,27 @@ struct SplashView: View {
                   
                     
                         VStack {
-                            HStack {
-                                Image("rlogo")
+                                                            Image("rlogo")
                                     .resizable()
                                     .frame(width: getRelativeWidth(201.0),
-                                           height: getRelativeHeight(62.0), alignment: .center)
+                                           height: getRelativeHeight(62.0))
                                     .scaledToFit()
                                     .clipped()
-                                    .padding(.vertical, getRelativeHeight(28.0))
+                             
                                 Spacer()
                                 Image("i")
                                     .resizable()
                                     .frame(width: getRelativeWidth(70.0),
-                                           height: getRelativeHeight(70.0), alignment: .center)
+                                           height: getRelativeHeight(70.0))
                                     .scaledToFit()
-                                    .clipped()
-                            }
+                                    .clipped().padding(.horizontal)
+
+                            }.padding(.horizontal)
                             .frame(width: getRelativeWidth(285.0), height: getRelativeHeight(201.0),
                                    alignment: .trailing)
-                            .padding(.leading, getRelativeWidth(10.0))
-                            Image("img_group")
-                                .resizable()
-                                .frame(width: getRelativeWidth(293.0), height: getRelativeHeight(200.0),
-                                       alignment: .center)
-                                .scaledToFit()
-                                .clipped()
-                                .padding(.top, getRelativeHeight(112.0))
-                                .padding(.trailing, getRelativeWidth(10.0))
-                    }
+                         
+              
+                    
                    
                 }.frame(width: 300, height: 700 ).padding(.all, 2).onAppear {
                     withAnimation(.easeInOut(duration: 0.5)) {
