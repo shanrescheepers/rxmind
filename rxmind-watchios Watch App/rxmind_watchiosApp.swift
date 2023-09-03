3fa92710b836cb95d7f66cb9863be7441cb2a933
Shanre Scheepers <21100387@virtualwindow.co.za>
2023-09-03 20:55:32 +0200

    // widget finito

---
+++ rxmind-watchios Watch App/rxmind_watchiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  rxmind_watchiosApp.swift
+//  rxmind-watchios Watch App
+//
+//  Created by Mac on 2023/09/03.
+//
+
+import SwiftUI
+
+@main
+struct rxmind_watchios_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
